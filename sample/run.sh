571a0365270cedbd00490e4558284a0a855998eb
Tomokazu Kiyohara <tomokazu.kiyohara@gmail.com>
2016-06-07 11:57:25 +0900

    mv uri_list.txt to .sample

--- sample/run.sh
+++ sample/run.sh
@@ -4,12 +4,19 @@ THIS_FILE_DIR=$(cd $(dirname $THIS_FILE_PATH); pwd)
 ######################################################################
 
 cd $THIS_FILE_DIR
-../exe/assets_domain_checker bulk_list --file uri_list.txt > list.ltsv &&
-../exe/assets_domain_checker convert --file list.ltsv > conv.ltsv &&
-../exe/assets_domain_checker snip --file conv.ltsv --depth 2 > snip.ltsv &&
+
+URI_LIST=uri_list.txt
+if [ ! -e $URI_LIST ];then
+  URI_LIST=uri_list.txt.sample
+fi
+echo '**' $URI_LIST handling ...
+
+../exe/assets_domain_checker bulk_list --file uri_list.txt > _list.ltsv &&
+../exe/assets_domain_checker convert --file _list.ltsv > _conv.ltsv &&
+../exe/assets_domain_checker snip --file _conv.ltsv --depth 2 > _snip.ltsv &&
 
 echo '** tag base rating **'
-../exe/assets_domain_checker gbsnipped --file snip.ltsv
+../exe/assets_domain_checker gbsnipped --file _snip.ltsv
 
 echo '** domain base rating **'
-../exe/assets_domain_checker gbsnipped --file snip.ltsv --domain-count true
+../exe/assets_domain_checker gbsnipped --file _snip.ltsv --domain-count true
